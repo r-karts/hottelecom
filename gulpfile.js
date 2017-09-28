85c81ad5ce699d7a22790c6005c1af1d6c00a30c
Karts <9369992@gmail.com>
2017-09-29 00:35:25 +0300

    Setup structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
 
-gulp.task('default', function() {
-	return console.log("Testing gulp...");
+
+gulp.task("default", ["copyhtml", "imagemin", "minify", "sass"]);
+
+gulp.task("copyhtml", function() {
+	gulp.src("src/*.html")
+		.pipe(gulp.dest("dist/"));
+});
+
+gulp.task("imagemin", function() {
+	gulp.src("src/images/*")
+		.pipe(imagemin())
+		.pipe(gulp.dest("dist/images"));
+});
+
+gulp.task("minify", function() {
+	gulp.src("src/js/*")
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("sass", function() {
+	gulp.src("src/css/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("dist/css"))
 });
